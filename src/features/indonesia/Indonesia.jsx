0cb8319f6272bbf198763de0fcb51ed12a2e63fb
Rismafebriana <111462571+Rismafebriana@users.noreply.github.com>
2022-10-27 09:52:56 +0700

    Add files via upload

---
+++ src/features/indonesia/Indonesia.jsx
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const news_API_Indonesia =
+  "https://newsapi.org/v2/top-headlines?country=id&from=2022-09-30&sortBy=popularity&apiKey=ac65afee406647cbabb22c674ad4c642";
+
+const initialState = {
+  entities: [],
+  isPending: false,
+};
+
+export const fetchNews = createAsyncThunk("articles/fetchNews", async () => {
+  const response = await axios.get(news_API_Indonesia);
+  return response.data;
+});
+
+const newsSlice = createSlice({
+  name: "articles",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchNews.pending, (state) => {
+        state.isPending = true;
+      })
+
+      .addCase(fetchNews.fulfilled, (state, action) => {
+        state.entities = action.payload.articles;
+      });
+  },
+});
+
+export default newsSlice.reducer;
